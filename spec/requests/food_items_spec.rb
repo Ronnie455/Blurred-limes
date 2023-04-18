fba73f722e3334afcaa36ffe3a6db561f87378c6
Ronnie455 <ronniemaynard4@gmail.com>
2023-04-18 13:38:51 -0700

    naming conventions

--- spec/requests/food_items_spec.rb
+++ spec/requests/food_items_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe "FoodItems", type: :request do
       food_item = user.food_items.create(name: "Milk", quantity: 1, expiration_date: "05/01/2023", location: "Refrigerator", image: "https://images.unsplash.com/photo-1563636619-e9143da7973b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1pbGt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60")
 
       get '/food_items'
-      apartment = JSON.parse(response.body)
+      food_item = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(apartment.length).to eq 1
+      expect(food_item.length).to eq 1
     end
   end
 end
