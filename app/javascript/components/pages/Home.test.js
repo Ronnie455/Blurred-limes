54e91946f5f7ae636ea08955946f9fc91b3f5ca3
Manflo27 <maflores1987@yahoo.com>
2023-04-18 14:32:36 -0700

    updated testing for home

--- app/javascript/components/pages/Home.test.js
+++ app/javascript/components/pages/Home.test.js
@@ -1,4 +1,4 @@
-import { render} from '@testing-library/react';
+import { render,screen} from '@testing-library/react';
 import Home from './Home.js';
 import { BrowserRouter } from "react-router-dom"
 import "@testing-library/jest-dom"
@@ -6,19 +6,23 @@ import React from 'react';
 
 
 describe("<Home />", () => {
-  it("renders without crashing", () => {
+  beforeEach(() => {
     render(
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
+        <BrowserRouter>
+            <Home/>
+        </BrowserRouter>
     )
   })
-  it('displays text home', () => {
-    render(
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    )
-    expect('home').toBeInTheDocument
+  it("renders without crashing", () => {   
+    screen.logTestingPlaygroundURL()
   })
-})
+  it("has a greeting", () => {
+    const header = screen.getByRole('heading', {name:/welcome!/i})
+    expect(header).toBeInTheDocument()
+  })
+  it("has a description", () => {
+    const description = screen.getByText(
+      /tired of buying items you already have at home\? have you ever had a roommate eat your food\? do ya'll wish you could keep your groceries more organized\? well\.\.\.\.we are here to clear up the blurred limes!! with our new and innovative application blurred limes you can reduce waste and save money by keeping a log of personal inventory which will save you time and keep your kitchen organized\. so sign up today because no one wants an angry kitchen\./i)
+      expect(description).toBeInTheDocument()
+  })
+  })
