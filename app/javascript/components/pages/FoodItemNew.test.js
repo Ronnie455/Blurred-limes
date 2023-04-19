9239dcf66924a3217c7cc466f71155d9e1c1c854
tammytran96 <tammytran96@gmail.com>
2023-04-19 13:02:37 -0700

    completes create functionality with tests, controller method, validations, and form

--- app/javascript/components/pages/FoodItemNew.test.js
+++ app/javascript/components/pages/FoodItemNew.test.js
@@ -1,1 +1,51 @@
 import "@testing-library/jest-dom"
+import React from "react"
+import {render, screen} from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import FoodItemNew from "./FoodItemNew"
+
+describe("<FoodItemNew />", () => {
+  beforeEach(() => {
+    const current_user = {
+      email: "trfmalpha@gmail.com",
+      password: "testing123",
+      id: 1
+    }
+
+    render(
+      <BrowserRouter>
+        <FoodItemNew current_user={current_user}/>
+      </BrowserRouter>
+    )
+  })
+
+  it("renders without crashing", () => {screen.logTestingPlaygroundURL()})
+
+  it("has a textbox for name", () => {
+    const foodItem = screen.getByRole('textbox', {
+      name: /name/i
+    })
+    expect(foodItem).toBeInTheDocument()
+  })
+
+  it("has a textbox for quantity", () => {
+    const foodItem = screen.getByRole('spinbutton', {name: /quantity/i})
+    expect(foodItem).toBeInTheDocument()
+  })
+
+  it("has a textbox for expiration_date", () => {
+    const foodItem = screen.getByPlaceholderText(/good until what date\?/i)
+    expect(foodItem).toBeInTheDocument()
+  })
+
+  it("has a textbox for location", () => {
+    const foodItem = screen.getByPlaceholderText(/location/i)
+    expect(foodItem).toBeInTheDocument()
+  })
+
+  it("has a textbox for image", () => {
+    const foodItem = screen.getByPlaceholderText(/Add an image URL/i)
+    expect(foodItem).toBeInTheDocument()
+  })
+
+})
