4c05fd3b8f22c906ad3a25c1faa07a84cc22b978
Marilyn Remigio <marilynremigio@gmail.com>
2023-04-28 11:08:01 -0600

    updates links on about us, updates ronnies photo, adds back to home link on log in page

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -4,7 +4,7 @@ import Icon from "../assets/Icon.png"
 const Footer = () => {
   return (
     <div className='footer'>
-      <img style={{ width: 75, height: 50 }} src={Icon} alt='photo of logo icon' className='header-logo'/>  © 2023 Blurred Limes | <a style={{color:"#203611"}} href='https://github.com/marilynremigio'>Marilyn Remigio</a> | <a style={{color:"#203611"}} href='https://github.com/tammytran96'>Tammy Tran</a> | <a style={{color:"#203611"}} href='http://github.com/Manflo27'>Manny Flores</a> | <a style={{color:"#203611"}} href='https://github.com/Ronnie455'>Ronnie Maynard</a>
+      <img style={{ width: 75, height: 50 }} src={Icon} alt='photo of logo icon' className='header-logo'/> <a target="_blank" style={{color:"#203611"}} href='https://github.com/TRFM-Radio/blurred-limes'>© 2023 Blurred Limes</a> | <a target="_blank" style={{color:"#203611"}} href='https://github.com/marilynremigio'>Marilyn Remigio</a> | <a target="_blank" style={{color:"#203611"}} href='https://github.com/tammytran96'>Tammy Tran</a> | <a target="_blank" style={{color:"#203611"}} href='http://github.com/Manflo27'>Manny Flores</a> | <a target="_blank" style={{color:"#203611"}} href='https://github.com/Ronnie455'>Ronnie Maynard</a>
     </div>
   )
 }
